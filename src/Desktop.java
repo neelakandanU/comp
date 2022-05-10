0030efc269101b2440bc9aa77c55de91b0549b60
neelakandanU <neelakandan2k16@gmail.com>
2022-05-10 17:39:32 +0530

    file push

---
+++ src/Desktop.java
@@ -0,0 +1,30 @@
+
+public class Desktop implements HardWare,SoftWare {
+
+	@Override
+	public void softWareResources() {
+		
+		System.out.println("Software Resource:");
+		
+	}
+
+	@Override
+	public void hardWareResources() {
+		System.out.println("Hardware Resource");
+		
+	}
+	public void desktopModel() {
+		
+		System.out.println("Desktop Model");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Desktop e = new Desktop();
+		e.softWareResources();
+		e.hardWareResources();
+		e.desktopModel();
+	}
+}
+
